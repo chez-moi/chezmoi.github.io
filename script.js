2a7310fd2ea9ff32e1cb5a7264d7d98109752d2e
chez-moi <30391541+chez-moi@users.noreply.github.com>
2017-08-03 00:15:10 +0200

    test

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-console.log('toto');
-alert('Votre iphone va exploser!');
-
-var myArray = ['1'];
-
-
-for(var i = 0; i < 10000000000; i++ ) {
-  for(var i = 0; i < myArray.length; i++ ) {
-    myArray.push(i);
-    console.log('counter' + i);
-    alert('Votre iphone rame....');
-  }
-}
+console.log('chargé');

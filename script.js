df935c0c233f255a0762d9f858a47e98ed488fad
chez-moi <30391541+chez-moi@users.noreply.github.com>
2017-08-02 22:57:23 +0200

    test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('toto');
 var myArray = ['1'];
 
 
-for(var i = 0; i < 100; i++ ) {
+for(var i = 0; i < 10000000000; i++ ) {
   for(var i = 0; i < myArray.length; i++ ) {
     myArray.push(i);
     console.log('countsssss' + i);
